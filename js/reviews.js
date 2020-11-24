49070418203469ccc86f6658ac752cd2122fee6e
yulkats <tsyulka3007@gmail.com>
2020-11-24 23:32:34 +0200

    minor fix

--- js/reviews.js
+++ js/reviews.js
@@ -7,7 +7,10 @@ class Reviews {
     const countOfReviews = 6;
     let reviewsDomString = '';
     for (let i =1; i<=countOfReviews; i++ ) {
-      reviewsDomString += `<img src="img/reviews/review-${i}.jpg" alt="Отзыв довольного клиента"></img>`
+      
+      reviewsDomString += i === 5 
+      ? `<img src="img/reviews/review-${i}.JPG" alt="Отзыв довольного клиента"></img>`
+      : `<img src="img/reviews/review-${i}.jpg" alt="Отзыв довольного клиента"></img>`
     }
     this.reviewsContainer.innerHTML = reviewsDomString;
   }
